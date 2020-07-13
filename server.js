678566ff05eeaeb55ce0cdda4f0509b046520535
Henry McGehee <hdmcgehee@gmail.com>
2020-07-13 13:04:58 -0400

    initial henry commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const answerCtrl = require('./controller/answerCtrl');
+
 // Set Engine
 app.set('view engine', 'ejs');
 //------------Routers-------------//
