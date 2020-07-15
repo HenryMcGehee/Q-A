550e67dd378c836a17416b7534130d0df4e16fc6
Henry McGehee <hdmcgehee@gmail.com>
2020-07-15 17:03:47 -0400

    auth follow along part 2

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -9,10 +9,60 @@ router.get('/login', (req, res) => {
     res.render('/user/login');
 });
 
+router.get('/profile', (req, res) => {
+    db.User.findById(req.session.currentUser._id, (err, foundUser) => {
+        if (err) return console.log(err);
+
+        res.render('/user/profile', {
+            user: foundUser,
+        });
+    })
+
+});
+
 router.get('/register', (req, res) => {
     res.render('/user/register');
 });
 
+//login
+router.post('/login', (req, res) => {
+    // Verify req.body Is Not Empty
+  // Find One User By Email
+  // If No User Found, Respond with 400
+  // Compare Password Sent Password and foundUser Password
+  // If Passwords Match, Create Session and Respond with 200
+  // If Passwords Do Not Match, Respond with 400
+  // Find User By Email Address
+  db.User.findOne({email: req.body.email}, (err, foundUser) => {
+    if (err) return console.log(err);
+    // Respond with 400 If No User Found
+    if (!foundUser) {
+      return res.send('No User Found');
+    }
+    // Compare User Password with foundUser Password
+    bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+      if (err) return console.log(err);
+      // Create Session and Respond with 200 If Passwords Match
+      if (isMatch) {
+        // Create currentUser Object (Hide User Password)
+        const currentUser = {
+          _id: foundUser._id,
+          username: foundUser.username,
+          email: foundUser.email,
+          isLoggedIn: true,
+        }
+        // Create A New Session and Respond 200
+        req.session.currentUser = currentUser;
+        res.redirect('/profile');
+      } else {
+        // Respond with 400 If Passwords Do Not Match
+        return res.send('Passwords do not match');
+      }
+    });
+  });
+})
+
+//register
 router.post('/register', (req, res) => {
     // Verify req.body Is Not Empty
   // Query DB For Existing User By Email
@@ -63,8 +113,16 @@ router.post('/register', (req, res) => {
 })
 
 
+//logout
+router.get('/logout', (req, res) => {
+    if (!req.session.currentUser) return res.redirect('/login');
 
+    req.session.destroy((err) => {
+        if (err) return console.log(err);
 
+        res.redirect('/login');
+    });
+});
 
 
 module.exports = router;
